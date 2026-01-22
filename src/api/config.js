40959886f4b15399c9ea8e243b2edf029ceb5ee8
Matthew <8951670+tthmatt@users.noreply.github.com>
2026-01-23 07:13:46 +0800

    change ip in config.js

--- src/api/config.js
+++ src/api/config.js
@@ -6,17 +6,17 @@ const CURRENT_CONFIG = {
   appLicense: process.env.REACT_APP_APP_LICENSE, // You need to go to the development website to apply.
 
   // http
-  baseURL: 'http://10.5.3.82:6789/', // This url must end with "/". Example: 'http://192.168.1.1:6789/'
-  websocketURL: 'ws://10.5.3.82:6789/api/v1/ws',  // Example: 'ws://192.168.1.1:6789/api/v1/ws'
+  baseURL: 'http://3.1.11.194:6789/', // This url must end with "/". Example: 'http://192.168.1.1:6789/'
+  websocketURL: 'ws://3.1.11.194:6789/api/v1/ws',  // Example: 'ws://192.168.1.1:6789/api/v1/ws'
 
   // livestreaming
   // RTMP  Note: This IP is the address of the streaming server. If you want to see livestream on web page, you need to convert the RTMP stream to WebRTC stream.
-  rtmpURL: 'rtmp://10.5.3.82/live/',  // Example: 'rtmp://192.168.1.1/live/' 
+  rtmpURL: 'rtmp://3.1.11.194/live/',  // Example: 'rtmp://192.168.1.1/live/' 
   // GB28181 Note:If you don't know what these parameters mean, you can go to Pilot2 and select the GB28181 page in the cloud platform. Where the parameters same as these parameters.
 
 }
 
-const HTTP_PREFIX = 'http://10.5.3.82:6789/manage/api/v1'
+const HTTP_PREFIX = 'http://3.1.11.194:6789/manage/api/v1'
 
 export {
   HTTP_PREFIX,
